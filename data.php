2a8ee1328bd4419818e49def86c486a16d7fe383
Amr Alaaeldin <80746926+amralaaeledin@users.noreply.github.com>
2022-02-09 08:21:06 +0200

    Update data.php

--- data.php
+++ data.php
@@ -58,7 +58,7 @@ $json_data = json_encode($data);
 
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
-header("Access-Control-Allow-Methods: *");
+header("Access-Control-Allow-Methods: GET");
 header("Access-Control-Allow-Headers: *");
 print_r($json_data);
 
