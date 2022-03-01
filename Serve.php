9324cef52c98f0a71f297aadb46c374afe0bdcb6
Amr Alaaeldin <80746926+amralaaeledin@users.noreply.github.com>
2022-03-01 11:58:36 +0200

    Update and rename serve.php to Serve.php

--- Serve.php
+++ Serve.php
@@ -1,5 +1,5 @@
 
-class serve
+class Serve
 {
   private $xml_data = null;
 
@@ -11,13 +11,13 @@ class serve
     header("Access-Control-Allow-Headers: *");
 
     if ($format === 'json') {
-      $json_data = json_encode(handling_resources::$data);
+      $json_data = json_encode(Handling_Resources::$data);
       print_r($json_data);
     }
 
     if ($format === 'xml') {
       $this->xml_data = new SimpleXMLElement('<?xml version="1.0"?><data></data>');
-      handling_resources::array_to_xml(handling_resources::$data, $this->xml_data);
+      Handling_Resources::array_to_xml(Handling_Resources::$data, $this->xml_data);
       print_r($this->xml_data->asXML());
     }
   }
