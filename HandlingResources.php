2dd580505e270e4653bde5848fdf72c6213572ba
Amr Alaaeldin <80746926+amralaaeledin@users.noreply.github.com>
2022-03-26 13:03:21 +0200

    Update HandlingResources.php

--- HandlingResources.php
+++ HandlingResources.php
@@ -45,35 +45,4 @@ class HandlingResources
     }
     return $data;
   }
-
-  private function arrayToXml($data, $xml_data)
-  {
-    foreach ($data as $key => $value) {
-      if (is_array($value)) {
-        if (is_numeric($key)) {
-          $key = 'item' . $key;
-        }
-        $sub_node = $xml_data->addChild($key);
-        $this->arrayToXml($value, $sub_node);
-      } else {
-        $xml_data->addChild("$key", htmlspecialchars("$value"));
-      }
-    }
-  }
-
-  public function formatOutput()
-  {
-    if (isset($_GET['format'])) {
-      if ($_GET['format'] === 'json') {
-        $json_data = json_encode($this->data);
-        return $json_data;
-      }
-
-      if ($_GET['format'] === 'xml') {
-        $this->xml_data = new SimpleXMLElement('<?xml version="1.0"?><data></data>');
-        $this->arrayToXml($this->data, $this->xml_data);
-        return $this->xml_data->asXML();
-      }
-    }
-  }
 }
