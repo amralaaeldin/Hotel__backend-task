940cce94ced34044d19be9f16a895d69ce1e2f09
Amr Alaaeldin <80746926+amralaaeledin@users.noreply.github.com>
2022-04-04 17:35:29 +0200

    Add files via upload

---
+++ src/Format/FormatJson.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Format;
+
+class FormatJson extends FormatData
+{
+  public function formatData()
+  {
+    header("Content-Type: application/json; charset=UTF-8");
+
+    return json_encode($this->data);
+  }
+}
