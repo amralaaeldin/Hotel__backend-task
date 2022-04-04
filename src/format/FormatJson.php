ee1f68451404db9cb3c8352d8f7e3f02dddffc98
Amr Alaaeldin <80746926+amralaaeledin@users.noreply.github.com>
2022-04-04 17:30:18 +0200

    Update FormatJson.php

--- src/format/FormatJson.php
+++ src/format/FormatJson.php
@@ -7,6 +7,7 @@ class FormatJson extends FormatData
   public function formatData()
   {
     header("Content-Type: application/json; charset=UTF-8");
+    
     return json_encode($this->data);
   }
 }
