7b802d0fa558d52cdd331e5e0f45fe23f7eadf4e
Amr Alaaeldin <80746926+amralaaeledin@users.noreply.github.com>
2022-04-05 13:18:10 +0200

    Update FormatFactory.php

--- src/FormatFactory.php
+++ src/FormatFactory.php
@@ -7,8 +7,8 @@ class FormatFactory
 
   public function create($data)
   {
-    $formatType = 'App\Format'.'\Format' . ucfirst($_GET['format']);
-    $format = new $formatType($data);
-    return $format->formatData();
+    $dataType = 'App\Format'.'\Format' . ucfirst($_GET['format']);
+    $type = new $dataType($data);
+    return $type->formatData();
   }
 }
